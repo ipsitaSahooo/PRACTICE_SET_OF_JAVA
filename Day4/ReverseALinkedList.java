2b412a677ad4802f867c31da1d72823a67976608
ipsitaSahooo <102987358+ipsitaSahooo@users.noreply.github.com>
2023-05-16 23:10:48 +0530

    Update ReverseALinkedList.java

--- Day4/ReverseALinkedList.java
+++ Day4/ReverseALinkedList.java
@@ -1,1 +1,37 @@
+import java.io.*;
+import java.util.* ;
 
+/*
+	Following is the structure of the Singly Linked List.	
+	class LinkedListNode<T> 
+    {
+    	T data;
+    	LinkedListNode<T> next;
+    	public LinkedListNode(T data) 
+        {
+        	this.data = data;
+    	}
+	}
+
+*/
+public class Solution 
+{
+    public static LinkedListNode<Integer> reverseLinkedList(LinkedListNode<Integer> head) 
+    {
+		if(head==null || head.next==null)
+		{
+			return head;
+		}
+		LinkedListNode<Integer> prev=null;
+		LinkedListNode<Integer> curr=head;
+		LinkedListNode<Integer> forward=null;
+		while(curr!=null)
+		{
+			forward=curr.next;
+			curr.next=prev;
+			prev=curr;
+			curr=forward;
+		}
+		return prev;
+    }
+}
