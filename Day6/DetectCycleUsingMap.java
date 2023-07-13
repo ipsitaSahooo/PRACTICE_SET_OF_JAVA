b0dccb121bf1d66e0040a1bc492d6413d6f5d348
ipsitaSahooo <102987358+ipsitaSahooo@users.noreply.github.com>
2023-07-13 20:42:51 +0530

    Update DetectCycleUsingMap.java

--- Day6/DetectCycleUsingMap.java
+++ Day6/DetectCycleUsingMap.java
@@ -1,1 +1,24 @@
+public static boolean loopDetect(Node head)
+  {
+  if(head==null)
+  return true;
 
+map<Node,boolean> visited;
+
+Node temp=head;
+while(temp!=null)
+  {
+    if(visited[temp]==true)
+    {
+      // to print the node where loop started
+      //System.out.println(temp.data);
+      return true;
+    }
+visited[temp]=true;
+    temp=temp.next;
+  }
+return false;
+}
+
+
+//Time complexity O(n)
