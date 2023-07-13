a1c970e25a5a256b62eae9c42b5402d86cbec909
ipsitaSahooo <102987358+ipsitaSahooo@users.noreply.github.com>
2023-07-13 21:30:07 +0530

    Update RemoveLoop.java

--- Day6/RemoveLoop.java
+++ Day6/RemoveLoop.java
@@ -1,1 +1,34 @@
+// to find the node where lopp started
+public static Node startLoop(Node head)
+  {
+  if(head==null)
+  return true;
 
+Node intersection=DetectLoop(head);
+Node slow=head;
+
+while(slow!=intersection)
+  {
+    slow=slow.next;
+    intersection=intersection.next;
+  }
+return slow;
+}
+
+//remove loop
+
+public static void removeLoop(Node head)
+  {
+  if(head==null)
+  return;
+
+Node start=startLoop(head);
+Node temp=start;
+while(temp.next!=start)
+  {
+    temp=temp.next;
+  }
+temp.next=null;
+}
+
+//Time complexity = O(n)
