7ec727daf548d7bc19e161948065424a40ddde2b
ipsitaSahooo <102987358+ipsitaSahooo@users.noreply.github.com>
2023-07-14 22:50:18 +0530

    Update RemoveDuplicateFromUnsortedLL.java

--- Day7/RemoveDuplicateFromUnsortedLL.java
+++ Day7/RemoveDuplicateFromUnsortedLL.java
@@ -1,1 +1,45 @@
+import java.util.* ;
+import java.io.*; 
 
+/************************************************************
+
+    Following is the linked list node structure
+    
+    class LinkedListNode<T> {
+    T data;
+    LinkedListNode<T> next;
+
+    public LinkedListNode(T data) {
+        this.data = data;
+    }
+}
+
+************************************************************/
+
+
+public class Solution {
+	
+	public static LinkedListNode<Integer> removeDuplicates(LinkedListNode<Integer> head) {
+
+        HashSet<Integer> hs = new HashSet<>();
+ 
+        /* Pick elements one by one */
+        LinkedListNode<Integer> current = head;
+        LinkedListNode<Integer> prev = null;
+        while (current != null) {
+            int curval = current.data;
+ 
+            // If current value is seen before
+            if (hs.contains(curval)) {
+                prev.next = current.next;
+            }
+            else {
+                hs.add(curval);
+                prev = current;
+            }
+            current = current.next;
+        }
+        return head;
+	}
+
+}
