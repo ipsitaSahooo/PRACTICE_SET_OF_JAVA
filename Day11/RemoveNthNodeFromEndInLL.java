ba9f7f32d63b6e48d1d3ab5286f2b116c79a7f5f
ipsitaSahooo <102987358+ipsitaSahooo@users.noreply.github.com>
2023-07-18 20:43:27 +0530

    Update RemoveNthNodeFromEndInLL.java

--- Day11/RemoveNthNodeFromEndInLL.java
+++ Day11/RemoveNthNodeFromEndInLL.java
@@ -1,1 +1,48 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) 
+    {
+        if(head==null || head.next==null)
+        return null;
 
+        int size=0;
+        ListNode temp=head;
+
+        while(temp!=null)
+        {
+            temp=temp.next;
+            size++;
+        }
+
+        int target=size-n;
+
+        temp=head;
+        int i=1;
+        if(target<=0)
+        {
+            head=head.next;
+            return head;
+        }
+        while(i!=target)
+        {
+           temp=temp.next;
+            i++;
+        }
+
+        if(temp.next!=null)
+        temp.next=temp.next.next;
+        else
+        temp.next=null;
+        
+        return head;
+    }
+}
