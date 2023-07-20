a8381611b2fed5dc880998e153e9fe0197858d9b
ipsitaSahooo <102987358+ipsitaSahooo@users.noreply.github.com>
2023-07-20 20:45:12 +0530

    Update RotateLLbyK.java

--- Day13/RotateLLbyK.java
+++ Day13/RotateLLbyK.java
@@ -1,1 +1,35 @@
-
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode rotateRight(ListNode head, int k) {
+         if (head == null) {
+            return head;
+        }
+        int len = 1;
+        ListNode temp = head;
+        while (temp.next != null) {
+            temp = temp.next;
+            len++;
+        }
+        k = k % len;
+        if (k == 0) {
+            return head;
+        }
+        temp.next = head;
+        ListNode newHead = head;
+        for (int i = 0; i < len - k - 1; i++) {
+            newHead = newHead.next;
+        }
+        ListNode temp2 = newHead.next;
+        newHead.next = null;
+        return temp2;
+    }
+}
