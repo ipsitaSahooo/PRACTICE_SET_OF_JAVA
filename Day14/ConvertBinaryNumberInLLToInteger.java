31b50e3c5cd9cb9badc1e5b05fe3f066ff73ff55
ipsitaSahooo <102987358+ipsitaSahooo@users.noreply.github.com>
2023-07-21 20:39:48 +0530

    Update ConvertBinaryNumberInLLToInteger.java

--- Day14/ConvertBinaryNumberInLLToInteger.java
+++ Day14/ConvertBinaryNumberInLLToInteger.java
@@ -1,1 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public int getDecimalValue(ListNode head) 
+    {
+        StringBuilder s=new StringBuilder();
+        ListNode temp=head;
+        while(temp!=null)
+        {
+            s.append(temp.val);
+            temp=temp.next;
+        }
+        int ans=Integer.parseInt(s.toString(),2);
 
+        return ans;
+    }
+}
