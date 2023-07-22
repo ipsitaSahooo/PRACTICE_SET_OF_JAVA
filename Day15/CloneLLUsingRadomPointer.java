8def241981dea430e8a13ccd4ef10486d89d8f6e
ipsitaSahooo <102987358+ipsitaSahooo@users.noreply.github.com>
2023-07-22 11:35:35 +0530

    Update CloneLLUsingRadomPointer.java

--- Day15/CloneLLUsingRadomPointer.java
+++ Day15/CloneLLUsingRadomPointer.java
@@ -1,1 +1,43 @@
+/*
+// Definition for a Node.
+class Node {
+    int val;
+    Node next;
+    Node random;
 
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
+*/
+
+class Solution {
+    public Node copyRandomList(Node head) 
+    {
+        HashMap<Node , Node> m=new HashMap<>();
+        Node curr=head;
+
+        while(curr!=null)
+        {
+            Node temp=new Node(curr.val);
+            m.put(curr,temp);
+            curr=curr.next; 
+        }
+
+        curr=head;
+        while(curr!=null)
+        {
+            Node temp=m.get(curr);
+            temp.next=m.get(curr.next);
+            temp.random=m.get(curr.random);
+            curr=curr.next;
+        }
+
+        return m.get(head);
+    }
+}
+
+//TC=O(N)
+//SC=O(N)
