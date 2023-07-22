0fa3f0a2180277acb0740017c16a291095e20037
ipsitaSahooo <102987358+ipsitaSahooo@users.noreply.github.com>
2023-07-22 11:57:26 +0530

    Update LengthOfLastWord.java

--- Day15/LengthOfLastWord.java
+++ Day15/LengthOfLastWord.java
@@ -1,1 +1,9 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        String[] words = s.split(" ");
+        return words[words.length - 1].length();
+    }
+}
 
+//TC=O(1)
+//SC=O(1)
