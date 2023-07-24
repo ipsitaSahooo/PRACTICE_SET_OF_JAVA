ed207cdf960fbb645393a818716cb9f55e315865
ipsitaSahooo <102987358+ipsitaSahooo@users.noreply.github.com>
2023-07-24 11:17:46 +0530

    Create SetBit(class).java

---
+++ Day17/SetBit(class).java
@@ -0,0 +1,28 @@
+package prePlacementClass;
+import java.util.*;
+
+//reset operation means convert 1 to 0
+//set operation means convert 0 to 1
+//Ex- n=10 , at pos=3 do set operation
+//n=10 = 1010 
+//here convert 3rd bit to 1
+// i.e ans is 1110
+
+public class SetBit {
+
+	public static void main(String[] args) 
+	{
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		int pos=s.nextInt();
+		
+		int mask=1<<(pos-1);
+		
+		int ans= n|mask;
+		
+		System.out.println(ans);
+		
+
+	}
+
+}
