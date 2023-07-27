53c7ca67050c21874c1ec734361b59d52367f702
ipsitaSahooo <102987358+ipsitaSahooo@users.noreply.github.com>
2023-07-27 20:11:24 +0530

    Update DeleteMiddleElementInStack(recursion).java

--- Day20/DeleteMiddleElementInStack(recursion).java
+++ Day20/DeleteMiddleElementInStack(recursion).java
@@ -1,1 +1,26 @@
 
+class Solution
+{
+    public static void solve (Stack<Integer> inputStack, int N, int count )
+	{
+		if (count==N/2)
+		{
+			inputStack.pop();
+			return;
+		}
+    
+		int n=inputStack.peek();
+		inputStack.pop();
+
+    //recursion
+		solve (inputStack, N, count+1);
+    
+		inputStack.push(n);
+	}
+    //Function to delete middle element of a stack.
+    public void deleteMid(Stack<Integer>s,int sizeOfStack){
+        // code here
+        int count=0;
+		solve (s, sizeOfStack, count);
+    }
+}
