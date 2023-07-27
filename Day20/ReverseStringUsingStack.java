6b8bc79f3975f1109df6519ceba89f407fb7d4d8
ipsitaSahooo <102987358+ipsitaSahooo@users.noreply.github.com>
2023-07-27 19:26:32 +0530

    Update ReverseStringUsingStack.java

--- Day20/ReverseStringUsingStack.java
+++ Day20/ReverseStringUsingStack.java
@@ -1,1 +1,20 @@
+class Solution {
+    public void reverseString(char[] s) 
+    {
+        Stack<Character> st=new Stack<>();
 
+        for(char ch:s)
+        {
+            st.push(ch);
+        }
+        int i=0;
+        while(!st.isEmpty())
+        {
+            s[i]=st.pop();
+            i++;
+        }
+    }
+}
+
+//O(N)=TC
+//O(1)=SC
