700f7e775f4b7ae63c00a5868eb377752cfcf724
ipsitaSahooo <102987358+ipsitaSahooo@users.noreply.github.com>
2023-07-31 20:21:45 +0530

    Update SQRTX.java

--- DAY24/SQRTX.java
+++ DAY24/SQRTX.java
@@ -1,1 +1,20 @@
-
+class Solution {
+    public int mySqrt(int x) {
+        if (x==0)
+            return 0;
+        int first =1;
+        int last=x;
+        while (first<=last )
+        {
+            int mid=first+((last-first)/2);
+            int half=x/mid;
+            if (mid==half)
+                return mid;
+            if (mid>half)
+                last=mid-1;
+            else 
+                first=mid+1;
+        }
+        return last;
+    }
+}
