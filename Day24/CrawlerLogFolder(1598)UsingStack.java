0bf069285c04ed0edd74a9d01c3226764cd5782f
ipsitaSahooo <102987358+ipsitaSahooo@users.noreply.github.com>
2023-08-03 22:20:36 +0530

    Update CrawlerLogFolder(1598)UsingStack.java

--- Day24/CrawlerLogFolder(1598)UsingStack.java
+++ Day24/CrawlerLogFolder(1598)UsingStack.java
@@ -1,1 +1,23 @@
+class Solution 
+{
+    public int minOperations(String[] logs) 
+  {
+        Stack<String> stack = new Stack<>();
+        for(String log : logs){
+            if(log.equals("../"))
+            {
+                if(!stack.empty())
+                    stack.pop();
+            }
+            else if(log.equals("./"))
+            {
 
+            }
+            else
+            {
+                stack.push(log);
+            }
+        }
+        return stack.size();
+    }
+}
