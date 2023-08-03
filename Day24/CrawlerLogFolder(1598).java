6f9ff018ef24547a7ca75b1f96282d8333aa75c7
ipsitaSahooo <102987358+ipsitaSahooo@users.noreply.github.com>
2023-08-03 22:17:29 +0530

    Update CrawlerLogFolder(1598).java

--- Day24/CrawlerLogFolder(1598).java
+++ Day24/CrawlerLogFolder(1598).java
@@ -1,1 +1,29 @@
-
+class Solution {
+    public int minOperations(String[] log) {
+      int count=0;
+        for(int i=0; i<log.length; i++)
+        {
+            
+            if(!log[i].equals("./") && !log[i].equals("../"))
+            {
+                count++;
+            }
+            else if(log[i].equals("./"))
+            {
+                continue;
+            }
+            else
+            {
+                if(count!=0)
+                {
+                count--;
+                }
+            }
+        }
+        if(count<=0)
+        {
+            return 0;
+        }
+        return count;
+    }
+}
