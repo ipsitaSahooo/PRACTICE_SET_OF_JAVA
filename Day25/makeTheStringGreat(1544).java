162f0ffe62e15c592c3bc1cd7cbc05aa9a5688df
ipsitaSahooo <102987358+ipsitaSahooo@users.noreply.github.com>
2023-08-04 21:00:12 +0530

    Update makeTheStringGreat(1544).java

--- Day25/makeTheStringGreat(1544).java
+++ Day25/makeTheStringGreat(1544).java
@@ -1,1 +1,14 @@
-
+class Solution {
+    public String makeGood(String s) 
+    {
+       StringBuilder sb = new StringBuilder();
+        for(char c : s.toCharArray())
+         {
+            if(!sb.isEmpty() && Math.abs(c - sb.charAt(sb.length()-1)) == 32)
+                sb.deleteCharAt(sb.length()-1);
+            else
+                sb.append(c);
+        }
+        return sb.toString();
+    }
+}
