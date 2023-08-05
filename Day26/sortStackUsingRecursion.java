2104725ebffcce5e87c8f7dbacf06558e5268075
ipsitaSahooo <102987358+ipsitaSahooo@users.noreply.github.com>
2023-08-05 19:30:39 +0530

    Create sortStackUsingRecursion.java

---
+++ Day26/sortStackUsingRecursion.java
@@ -0,0 +1,35 @@
+import java.util.* ;
+import java.io.*; 
+public class Solution 
+{
+	public static void sortedInsert(Stack<Integer> stack,int x)
+	{
+		if((stack.isEmpty()) || (!stack.isEmpty() && stack.peek()<x))
+		{
+			stack.push(x);
+			return;
+	    }
+		int n=stack.peek();
+		stack.pop();
+
+		//recursiveCall
+		sortedInsert(stack,x);
+
+		stack.push(n);
+	}
+
+	public static void sortStack(Stack<Integer> stack) 
+	{
+		if(stack.isEmpty())
+		return;
+
+		int num=stack.peek();
+		stack.pop();
+
+		//recursiveCall
+		sortStack(stack);
+
+		sortedInsert(stack,num);
+	}
+
+}
