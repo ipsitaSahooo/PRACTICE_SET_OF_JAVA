4a906fa583ab770c5536783bdcf6fc020e37c843
ipsitaSahooo <102987358+ipsitaSahooo@users.noreply.github.com>
2023-08-05 19:51:55 +0530

    Update RedundantBrackets.java

--- Day26/RedundantBrackets.java
+++ Day26/RedundantBrackets.java
@@ -1,1 +1,52 @@
+//redundant brackets are those brackets that are useless
+//eg- ((a*b))  -   the outer bractes are useless
+// so in case of redundant brackets return true else return false
 
+import java.util.* ;
+import java.io.*; 
+import java.util.ArrayList;
+
+public class Solution 
+{
+    public static boolean findRedundantBrackets(String s) 
+    {
+        Stack<Character> st=new Stack<>();
+
+        for(int i=0;i<s.length();i++)
+        {
+            char ch=s.charAt(i);
+
+            if(ch=='(' || ch=='+' || ch=='-' || ch=='*' || ch=='/')
+            {
+                st.push(ch);
+            }
+            else
+            {
+                //its either a closing bracket or a lowercase character
+                if(ch==')')
+                {
+                    boolean ans=true;
+
+                    while(st.peek()!='(')
+                    {
+                        char ch1=st.peek();
+                        if(ch1=='+' || ch1=='-' || ch1=='*' || ch1=='/')
+                        {
+                            ans=false;
+                        }
+                        st.pop();
+
+                    }
+                    if(ans==true)
+                    return true;
+
+                    st.pop();
+                }
+            }
+        }
+
+        return false;
+    }
+}
+
+//TC = O(n)
