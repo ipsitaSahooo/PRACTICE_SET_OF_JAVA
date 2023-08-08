92e61cdb7db7828bfc06000599173b80f77ac472
ipsitaSahooo <102987358+ipsitaSahooo@users.noreply.github.com>
2023-08-08 18:54:08 +0530

    Update rotateString(796).java

--- Day29/rotateString(796).java
+++ Day29/rotateString(796).java
@@ -4,3 +4,21 @@ lass Solution {
        return (s.length()==goal.length() && (s+s).contains(goal)); 
     }
 }
+
+OR(not so optimal)
+    
+import java.util.*;
+class Solution 
+{
+    public boolean rotateString(String s, String goal) 
+    {
+        int n=s.length();
+        for(int i=0;i<s.length();i++)
+        {
+            s=s.substring(1,n)+s.charAt(0);
+            if(s.equals(goal))
+            return true;
+        }
+        return false;
+    }
+}
