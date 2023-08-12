b38f5ef65f4a7eee73ff81c4dc4a1f9f280389ad
ipsitaSahooo <102987358+ipsitaSahooo@users.noreply.github.com>
2023-08-12 19:27:56 +0530

    Update firstUniqueCharInAString(387).java

--- Day33/firstUniqueCharInAString(387).java
+++ Day33/firstUniqueCharInAString(387).java
@@ -15,3 +15,22 @@ class Solution
 
     }
 }
+
+OR
+
+
+    HashMap<Character,Integer> map=new HashMap();
+        int count=0;
+        for (int i=0;i<s.length();i++)
+        {
+            if (map.containsKey(s.charAt(i))){
+                map.put(s.charAt(i),map.get(s.charAt(i))+1);
+            }
+            else map.put(s.charAt(i),1);
+        }
+        for (int i=0;i<s.length();i++)
+        {
+            if (map.get(s.charAt(i))==1)
+                return i;
+        }
+        return -1;
