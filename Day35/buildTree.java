2964ba1a87f39a734c055ae49c8d1a2ac78cd8f0
ipsitaSahooo <102987358+ipsitaSahooo@users.noreply.github.com>
2023-08-14 19:49:15 +0530

    Create buildTree.java

---
+++ Day35/buildTree.java
@@ -0,0 +1,13 @@
+public static Node buildTree(Node root)
+  {
+  Scanner s=new Scanner(System.in);
+ int data=s.nextInt();
+
+root=new Node(data);
+ if(data == -1)
+   return null;
+
+root.left=buildTree(root.left);
+root.right=buildTree(root.right);
+return root
+  }
