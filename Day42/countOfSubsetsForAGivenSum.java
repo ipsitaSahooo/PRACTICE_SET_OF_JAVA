b2bac397d6dfa59984c99ae7b219b343655636b2
ipsitaSahooo <102987358+ipsitaSahooo@users.noreply.github.com>
2023-08-22 12:07:46 +0530

    Create countOfSubsetsForAGivenSum.java

---
+++ Day42/countOfSubsetsForAGivenSum.java
@@ -0,0 +1,29 @@
+class Solution{
+
+	public int perfectSum(int arr[],int n, int sum) 
+	{ 
+	    int dp[][] = new int[n+1][sum + 1];
+        for (int i = 0; i <= n; i++)
+            {
+                dp[i][0] = 1;
+                
+            }
+        for (int i = 1; i <= sum; i++)
+           { 
+               dp[0][i] = 0;
+               
+           }
+        for (int i = 1; i <= n; i++) 
+        {
+            for (int j = 1; j <= sum; j++) 
+            {
+                if (arr[i-1]<=j)
+                    dp[i][j]=dp[i-1][j] + dp[i-1][j - arr[i-1]];
+                else 
+                    dp[i][j]=dp[i-1][j];
+            }
+        }
+        return dp[n][sum];
+
+	} 
+}
