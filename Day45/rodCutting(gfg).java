a65946f51e35215aa543d454914bd2000bbfc43f
ipsitaSahooo <102987358+ipsitaSahooo@users.noreply.github.com>
2023-08-25 13:32:29 +0530

    Update rodCutting(gfg).java

--- Day45/rodCutting(gfg).java
+++ Day45/rodCutting(gfg).java
@@ -13,7 +13,7 @@ public class Solution
        int dp[][]=new int[n+1][n+1];
        for(int i=0;i<n+1;i++)
        {
-           for(int j=0;j<n+1;j++) // same as length+1
+           for(int j=0;j<n+1;j++) // same as length.length+1
            {
                if(i==0 || j==0)
                dp[i][j]=0;
@@ -22,7 +22,7 @@ public class Solution
        
        for(int i=1;i<n+1;i++)
        {
-           for(int j=1;j<n+1;j++) // same as length+1
+           for(int j=1;j<n+1;j++) // same as length.length+1
            {
                if(length[i-1]<=j)
                dp[i][j]=Math.max(price[i-1]+dp[i][j-length[i-1]],dp[i-1][j]);
@@ -31,7 +31,7 @@ public class Solution
                dp[i][j]=dp[i-1][j];
            }
        }
-       return dp[n][n];//same as[n][length]
+       return dp[n][n];//same as[n][length.length]
 
 	}
 }
